6eec6ceea43357826d1261002c65a526ce9efe05
naar04 <naar2007@gmail.com>
2026-05-08 10:26:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pdfplumber
-import os
 
 app = Flask(__name__)
 
-# Allow frontend (GitHub Pages)
-CORS(app, origins="*")
+CORS(app)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "PDF Converter API Running"
 
-@app.route('/convert', methods=['POST'])
+@app.route("/convert", methods=["POST"])
 def convert():
 
     try:
-        if 'file' not in request.files:
-            return jsonify({"success": False, "error": "No file uploaded"}), 400
 
-        file = request.files['file']
+        if "file" not in request.files:
+            return jsonify({
+                "success": False,
+                "error": "No file uploaded"
+            })
+
+        file = request.files["file"]
 
         text = ""
 
         with pdfplumber.open(file) as pdf:
+
             for page in pdf.pages:
+
                 extracted = page.extract_text()
+
                 if extracted:
                     text += extracted + "\n"
 
@@ -35,12 +40,11 @@ def convert():
         })
 
     except Exception as e:
+
         return jsonify({
             "success": False,
             "error": str(e)
-        }), 500
+        })
 
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# IMPORTANT FOR RENDER
+app = app
